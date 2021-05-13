d9ee007b8e61003f4395068ac3c057019fe8fc40
Nick Heymans <nick.heymans@student.hogent.be>
2021-05-13 18:47:07 +0200

    Dataverwerking + Conclusie uitgeschreven

--- bachproef/files/dataCpuGraph.R
+++ bachproef/files/dataCpuGraph.R
@@ -1,13 +1,18 @@
 library(readr)
 library(ggplot2)
-CPUoverTime <- read_table2("SC1_CpuGebruikDag9.csv", skip = 1)
+CPUoverTime <- read_table2("SC2_CPUGebruikDag12.csv", skip = 1)
+CPUoverTime <- head(CPUoverTime, - 2)
 
 #CPUoverTime <- CPUoverTime[1:8]
 names(CPUoverTime)[names(CPUoverTime) == "00:00:01"] <- "Tijd"
 names(CPUoverTime)[names(CPUoverTime) == "%idle"] <- "Cpu"
 
+CPUoverTime[8] <- 100 - CPUoverTime[8]
+
 CPUGraph <- ggplot(CPUoverTime, aes(Tijd,Cpu,group = 1)) + geom_line()
 
 CPUGraph <- CPUGraph + theme(axis.ticks.x = element_blank(), axis.text.x = element_blank())
+
+CPUGraph <- CPUGraph + coord_cartesian(ylim = c(5, 15))
 
 CPUGraph
--- bachproef/files/dataRAMGraph.R
+++ bachproef/files/dataRAMGraph.R
@@ -1,12 +1,11 @@
 library(readr)
 library(ggplot2)
-RAMUsage <- read_table2("SC1_RAMGebruikDag9.csv", skip = 2)
+RAMUsage <- read_table2("SC2_RAMGebruikDag12.csv", skip = 2)
 
 
 RAMUsage <- RAMUsage[-c(146, 147), ]
 names(RAMUsage)[names(RAMUsage) == "%memused"] <- "RAM"
 names(RAMUsage)[names(RAMUsage) == "00:00:01"] <- "Tijd"
-#sort(RAMUsage[Tijd])
 
 RAMGraph <- ggplot(RAMUsage, aes(Tijd,RAM,group = 1)) + geom_line() 
 
